e6e8c580d0774abd599295dd5a13ef86232f6217
Kandarp Kakkad <kandarp.b.kakkad@gmail.com>
2020-09-14 13:16:17 +0530

    Final Files

--- GUI.pl
+++ GUI.pl
@@ -3,47 +3,63 @@ use Tk;
 use strict;
 no warnings qw( experimental::smartmatch );
 
-my $mw = MainWindow->new;
+my $mw = MainWindow->new(-title => "CRUD Application");
+$mw->geometry( "1000x600" );
 
-$mw->Label(-text => 'Enter Directory Name')->pack;
+$mw = $mw->Frame(-borderwidth => 1.5, -relief => "groove")->pack(-side => "top", -fill => 'x');
+
+$mw->configure(-foreground => "black", -background => "white");
+
+$mw->Label(-text => 'Enter Directory Name')->pack(-side => "top")->configure(-foreground => "black", -background => "white");
 my $dirname = $mw->Entry(-width => 20);
-$dirname->pack;
-$mw->Label(-text => 'Enter New Directory Name For Renaming')->pack;
+$dirname->pack(-side => "top")->configure(-foreground => "black", -background => "white");
+
+$mw->Label(-text => '')->pack()->configure(-foreground => "black", -background => "white");
+
+$mw->Label(-text => 'Enter New Directory Name For Renaming')->pack(-side => "top")->configure(-foreground => "black", -background => "white");
 my $newdirname = $mw->Entry(-width => 20);
-$newdirname->pack;
+$newdirname->pack(-side => "top")->configure(-foreground => "black", -background => "white");
+
+$mw->Label(-text => '')->pack()->configure(-foreground => "black", -background => "white");
 
 $mw->Button(
     -text    => 'Create Directory',
     -command => sub {create_dir($dirname)},
-)->pack;
+)->pack(-side => "left")->configure(-foreground => "black", -background => "steelblue");
+
 $mw->Button(
     -text    => 'List all Director(y)(ies)',
     -command => sub { list_dir($dirname) },
-)->pack;
+)->pack(-side => "left")->configure(-foreground => "black", -background => "steelblue");
+
 $mw->Button(
     -text    => 'List all File(s)',
     -command => sub { list_file($dirname) },
-)->pack;
+)->pack(-side => "left")->configure(-foreground => "black", -background => "steelblue");
+
 $mw->Button(
     -text    => 'Rename Directory',
     -command => sub { rename_dir($dirname,$newdirname) },
-)->pack;
+)->pack(-side => "left")->configure(-foreground => "black", -background => "steelblue");
+
 $mw->Button(
     -text    => 'Delete a Directory',
     -command => sub { delete_dir($dirname) },
-)->pack;
+)->pack(-side => "left")->configure(-foreground => "black", -background => "steelblue");
+
+$mw->Label(-text => '')->pack(-side => "bottom")->configure(-foreground => "black", -background => "white");
+
 MainLoop;
+
 
 sub create_dir {
 	my ($dir_name) = @_;
 	my $dir = $dir_name->get;
 	if(mkdir($dir)){
-		$mw->Label(-text => "Directory created \n")->pack;
-		print "Directory created \n";
+		$mw->Label(-text => "Directory created \n")->pack(-side => "bottom")->configure(-foreground => "black", -background => "white");
 	}
 	else{
-		$mw->Label(-text => "'$dir' directory cannot be created.\n")->pack;
-		print "'$dir' directory cannot be created.\n";
+		$mw->Label(-text => "'$dir' directory cannot be created.\n")->pack(-side => "bottom")->configure(-foreground => "black", -background => "white");
 	}
 }
 
@@ -58,11 +74,11 @@ sub list_dir {
 		    $temp = $temp.$file;
 		    $temp = $temp."\n";
 		}   
-		$mw->Label(-text => "'$temp'")->pack;
+		$mw->Label(-text => "$temp")->pack(-side => "bottom")->configure(-foreground => "black", -background => "white");
 		closedir DIR; 
 	}
 	else{
-		$mw->Label(-text => 'No Directory, $!')->pack;
+		$mw->Label(-text => 'No Directory, ' + $dir)->pack(-side => "bottom")->configure(-foreground => "black", -background => "white");
 	}
 }
 
@@ -77,11 +93,11 @@ sub list_file {
 			$temp = $temp.$content;
 		    $temp = $temp."\n";
 		}
-		$mw->Label(-text => "'$temp'")->pack;
+		$mw->Label(-text => "$temp")->pack(-side => "bottom")->configure(-foreground => "black", -background => "white");
 		closedir $dh; 
 	}
 	else{
-		$mw->Label(-text =>  "Could not open '$dir' for reading '$!'\n")->pack;
+		$mw->Label(-text =>  "Could not open '$dir' for reading\n")->pack(-side => "bottom")->configure(-foreground => "black", -background => "white");
 	}
 }
 
@@ -89,10 +105,10 @@ sub delete_dir {
 	my ($dir_name) = @_;
 	my $dir = $dir_name->get;
 	if(rmdir($dir)){
-		$mw->Label(-text => "Directory removed \n")->pack;
+		$mw->Label(-text => "Directory removed \n")->pack(-side => "bottom")->configure(-foreground => "black", -background => "white");
 	}
 	else{
-		$mw->Label(-text => "Couldn't remove $dir directory, $!\n")->pack;   
+		$mw->Label(-text => "Couldn't remove $dir directory\n")->pack(-side => "bottom")->configure(-foreground => "black", -background => "white");   
 	}
 }
 
@@ -101,9 +117,9 @@ sub rename_dir {
 	my $dir = $dir_name->get;
 	my $newdir = $new_dir_name->get;
 	if(rename($dir,$newdir)){
-		$mw->Label(-text => "Directory renamed\n")->pack;
+		$mw->Label(-text => "Directory renamed\n")->pack(-side => "bottom")->configure(-foreground => "black", -background => "white");
 	}
 	else{
-		$mw->Label(-text => "Error in renaming")->pack;
+		$mw->Label(-text => "Error in renaming")->pack(-side => "bottom")->configure(-foreground => "black", -background => "white");
 	}
 }
